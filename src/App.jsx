9962c4de06d438fe7be87f901bb929091f47e2f8
Jesse Page <jesse.page@corebts.com>
2023-01-20 17:12:19 -0600

    completed react tutorial

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,26 @@
-import { React, useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { React } from 'react'
+import Navbar from './components/Navbar'
+import Hero from './components/Hero'
+import Card from './components/Card'
+import data from './data'
 
 export default function App() {
-  
+  const cards = data.map( item => {
+    return(
+      <Card
+        key={item.id}
+        {...item}
+      />
+    )
+  })
+
   return (
-    <nav>
-      <img src="src\images\airbnb-logo.png" width={100}/>
-    </nav>
+    <div>
+      <Navbar />
+      <Hero /> 
+      <section className="cards-list">
+          {cards}
+      </section>  
+    </div>
     )
   }
-  
-  // const [count, setCount] = useState(0)
-    // <div className="App">
-    //   <div>
-    //     <a href="https://vitejs.dev" target="_blank">
-    //       <img src="/vite.svg" className="logo" alt="Vite logo" />
-    //     </a>
-    //     <a href="https://reactjs.org" target="_blank">
-    //       <img src={reactLogo} className="logo react" alt="React logo" />
-    //     </a>
-    //   </div>
-    //   <h1>Vite + React</h1>
-    //   <div className="card">
-    //     <button onClick={() => setCount((count) => count + 1)}>
-    //       count is {count}
-    //     </button>
-    //     <p>
-    //       Edit <code>src/App.jsx</code> and save to test HMR
-    //     </p>
-    //   </div>
-    //   <p className="read-the-docs">
-    //     Click on the Vite and React logos to learn more
-    //   </p>
-    // </div>
-// export default App
